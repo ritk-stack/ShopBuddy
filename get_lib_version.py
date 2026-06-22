b94eb2c9286a75d15f63924dff2f05469b1a5286
Ritik <ritik@example.com>
2026-06-23 03:53:28 +0530

    chore: add pyproject and remaining utilities

---
+++ get_lib_version.py
@@ -0,0 +1,12 @@
+import importlib.metadata
+packages= [
+    "langchain",
+    "python-dotenv",
+    "langchain-core"
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}=={version}")
+    except importlib.metadata.PackageNotFoundError:
+        print(f"{pkg} (not installed)")    
